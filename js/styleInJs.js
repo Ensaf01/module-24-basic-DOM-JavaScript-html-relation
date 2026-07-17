af2b2dbff6ec96db1a8b87fcc8dbe378fbcae4aa
ensaf01 <mohammedensaf402@gmail.com>
2026-07-18 05:50:28 +0600

    dom ,styleIn.js,access html property via js

---
+++ js/styleInJs.js
@@ -0,0 +1,14 @@
+const sections=document.querySelectorAll('section');//use here tag 'section'for query
+//console.log(sections);
+for(const elements of sections){
+    //console.log(section);
+    elements.style.border='2px solid blue';
+    //element ==each section here
+    elements.style.borderRadius='15px';
+    elements.style.marginBottom='5px';
+    elements.style.backgroundColor='lightgreen';
+    elements.style.padding='10px';
+}
+
+const place=document.getElementById('place-id');
+place.style.backgroundColor='yellow';
